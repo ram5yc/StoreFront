ecc02f9b54258e43149f4b4ac24a3bfe776cc593
Rachel Mantei <ram5yc@mail.missouri.edu>
2021-02-20 10:03:50 -0600

    commit

--- StoreFront/StoreFront.UI.MVC/Models/ContactViewModel.cs
+++ StoreFront/StoreFront.UI.MVC/Models/ContactViewModel.cs
@@ -8,18 +8,22 @@ namespace StoreFront.UI.MVC.Models
 {
     public class ContactViewModel
     {
-        [Required(ErrorMessage = "* Your name is required.")]
+        [Required(ErrorMessage = "*Please provide a Name*")]
+        //All we are going to provie the class is the properties class member.
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "*Your email is required.")]
-        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "*Please provide an email*")]
+        [DataType(DataType.EmailAddress)] //        [RegularExpression("regex", ErrorMessage = "* Provide a valid email *")] is the same thing as datatype.emailaddress
+          //if you want to ensure your validatiocustomize the validation, you can still use a regular expression(regexlib.com).
+
         public string Email { get; set; }
 
         [Required(ErrorMessage = "*A subject is required.")]
         public string Subject { get; set; }
 
-        [UIHint("MulitilineText")]
-        [Required(ErrorMessage = "*A message is required.")]
+        [Required(ErrorMessage = "* Please provide a message *")]
+        [UIHint("MultilineText")]//changes the input to a textarea
         public string Message { get; set; }
+
     }
 }
